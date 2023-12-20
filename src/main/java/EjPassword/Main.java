e4c436ab56354c15caff3d91b3abd3aa85e0c036
alorenzot <alejandrolorenzotoledo@gmail.com>
2023-12-20 17:01:25 +0100

    Ejercicios

--- src/main/java/EjPassword/Main.java
+++ src/main/java/EjPassword/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Password p = new Password();
         System.out.print(p);
+        Password p2 = new Password(36);
+        System.out.println(p2);
     }
 }
--- src/main/java/EjPassword/Password.java
+++ src/main/java/EjPassword/Password.java
@@ -2,37 +2,87 @@ package EjPassword;
 
 public class Password {
     private int length;
-    private static int defaultLength=10;
+    private static int defaultLength = 10;
     private String password;
-    private String characters="abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789";
+    private boolean isStrong;
+    private String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789";
 
 
-    public Password(){
-        this.length=defaultLength;
-        this.password=generatePassword(length);
+    public Password() {
+        this.length = defaultLength;
+        this.password = generatePassword(length);
+        this.isStrong = isStrong();
     }
-    public Password(int length){
-        this.length=length;
-        this.password=generatePassword(length);
+    public Password(int length) {
+        this.length = length;
+        this.password = generatePassword(length);
+        this.isStrong = isStrong();
     }
-    public boolean isStrong(){
-        int strengthness=0;
-        for (int i=0; i<this.password.length();i++){
-            if (password.charAt(i)=='A');//mayuscula
-
-        }
-        return false;
+    public boolean isStrong() {
+        if (numberChecker(password)+lowerChecker(password)+upperChecker(password)==7) return true;
+        else return false;
     }
-    public String generatePassword(int length){
-        String aux="";
-        for (int i=0;i<=length;i++){
-            int random=(int)(Math.random()*characters.length());
-            aux+=characters.charAt(random);
+    private int numberChecker(String password) {
+        int check = 0;
+        if (password.length() != 0)
+            for (int i = 52; i < characters.length(); i++) {
+                for (int j = 0; j < password.length(); j++) {
+                    if (characters.charAt(i) == password.charAt(j)) {
+                        check++;
+                        if (check == 4) {
+                            return check;
+                        }
+                    }
+                }
+            }
+        return check;
+    }
+    private int lowerChecker(String password) {
+        int check = 0;
+        if (password.length() != 0)
+            for (int i = 0; i < 26; i++) {
+                for (int j = 0; j < password.length(); j++) {
+                    if (characters.charAt(i) == password.charAt(j)) {
+                        check++;
+                        if (check == 1) {
+                            return check;
+                        }
+                    }
+                }
+            }
+        return check;    }
+    public int upperChecker(String password) {
+        int check = 0;
+        if (password.length() != 0)
+            for (int i = 27; i < 52; i++) {
+                for (int j = 0; j < password.length(); j++) {
+                    if (characters.charAt(i) == password.charAt(j)) {
+                        check++;
+                        if (check == 2) {
+                            return check;
+                        }
+                    }
+                }
+            }
+        return check;    }
+    public String generatePassword(int length) {
+        String aux = "";
+        for (int i = 0; i < length; i++) {
+            int random = (int) (Math.random() * characters.length());
+            aux += characters.charAt(random);
         }
         return aux;
     }
+    public int getLength() {
+        return length;
+    }
+    public String getPasswordAndLength() {
+        return password + length;
+    }
     @Override
-    public  String toString(){
-        return "Your password is: "+password;
+    public String toString() {
+        return "Your password is: " + password + "\n" +
+                "Is strong: " + isStrong() + "\n"
+                +"\n";
     }
 }
