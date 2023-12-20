581564ef8cea2a8e71a3d18aeab5cbe535295ddf
alorenzot <alejandrolorenzotoledo@gmail.com>
2023-12-20 18:54:01 +0100

    Ejercicios

--- src/main/java/org/EjPersona/PersonaTester.java
+++ src/main/java/org/EjPersona/PersonaTester.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class PersonaTester {
     public static void main(String[] args) {
-        Persona[] personas = new Persona[1];
-        System.out.println("Vamos a guardar los datos personales de "+personas.length+" personas.");
+        Persona[] personas = new Persona[6];
+        System.out.println("Vamos a guardar los datos personales de " + personas.length + " personas.");
 
 
         for (int i = 0; i < personas.length; i++) {
