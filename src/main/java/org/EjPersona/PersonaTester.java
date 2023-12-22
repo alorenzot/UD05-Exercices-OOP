18fc742ec743576f79fe2d0309fbee952d451ad2
aleja <alejandrolorenzotoledo@gmail.com>
2023-12-22 18:37:13 +0100

    ejercicios

--- src/main/java/org/EjPersona/PersonaTester.java
+++ src/main/java/org/EjPersona/PersonaTester.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PersonaTester {
     public static void main(String[] args) {
-        Persona[] personas = new Persona[1];
+        Persona[] personas = new Persona[6];
 
         registrarPersonas(personas);
         mostrarPersonas(personas);
